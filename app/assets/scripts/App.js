140c064069af63f52b7d098292ecb67cf588040c
Eliseo <eliseotogz777@gmail.com>
2018-07-08 02:38:32 +0800

    JS file organization

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,14 @@
+var Person = require('./modules/Person');
+
+console.log(Person);
+
+/*var john = new Person("John Doe", "Blue");
+
+john.greet();
+
+var jane = new Person("Jane Doe", "Green");
+
+jane.greet();*/
+
+
+  
