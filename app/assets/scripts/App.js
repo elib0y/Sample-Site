04fa5393f8f52860f90491e7cf4529183d408bbd
Eli <eliseotogoz77@gmail.com>
2018-11-01 13:52:49 +0800

    completed reveal scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,9 @@
+//Import modules//
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
-var mobileMenu = new MobileMenu();  
+//new instance of class/modules//
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
